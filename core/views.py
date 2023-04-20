814e9795b020291d69441456c0160ae3803db08f
julius <julius.soltani@gmail.com>
2023-04-20 21:19:38 +0200

    Add google map api and corresponding design in footer

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
+    }
+    return render(request, 'index.html', context)
 
 def menu(request):
     return render(request, 'menu.html')
