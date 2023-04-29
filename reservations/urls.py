32a9c9333308fd0677619621dfd7c6e70a7ecab3
julius <julius.soltani@gmail.com>
2023-04-29 07:19:47 +0200

    Add basic structure for reservation model

--- reservations/urls.py
+++ reservations/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.reserve, name='reserve'),
+    path('reserve/', views.reserve, name='reserve'),
+    path('confirm/<int:reservation_id>/', views.confirm, name='confirm'),
+    path('admin_view/', views.admin_view, name='admin_view'),
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
+from .forms import ReservationForm
+from .models import Reservation
+
+def reserve(request):
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            reservation = form.save()
+            # Send confirmation email
+            # ...
+            return redirect('confirm', reservation_id=reservation.pk)
+    else:
+        form = ReservationForm()
+    return render(request, 'reserve.html', {'form': form})
+
+def confirm(request, reservation_id):
+    reservation = Reservation.objects.get(pk=reservation_id)
+    return render(request, 'confirm.html', {'reservation': reservation})
+
+def admin_view(request):
+    # Add authentication check
+    reservations = Reservation.objects.all().order_by('-date', '-time')
+    return render(request, 'admin_view.html', {'reservations': reservations})
 
 def reserve(request):
     context = {
