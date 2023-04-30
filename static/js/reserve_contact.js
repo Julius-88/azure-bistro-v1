1786ecde53f0620dfbc4a3b820cc91822a3d668e
julius <julius.soltani@gmail.com>
2023-04-30 17:53:47 +0200

    Adjusted some of the code so that emailjs works again

--- static/js/reserve_contact.js
+++ static/js/reserve_contact.js
@@ -13,30 +13,26 @@ window.onload = function () {
   document.getElementById('reservation_time').value = reservationTime;
   document.getElementById('reservation_guests').value = reservationGuests;
 
-  document
-    .getElementById('contact-form')
-    .addEventListener('submit', function (event) {
-      event.preventDefault();
-      // generate a five digit number for the contact_number variable
-      this.contact_number.value = (Math.random() * 100000) | 0;
+  document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault();
 
-      console.log('reservation_date:', this.reservation_date.value);
-      console.log('reservation_time:', this.reservation_time.value);
-      console.log('reservation_guests:', this.reservation_guests.value);
-      // emailjs.sendForm('service_azure', 'template_confirm', this).then(
-      //   function () {
-      //     alert('Your message has been sent!');
-      //     window.location.href = '/reserve/reserve/';
-      //   },
-      //   function (error) {
-      //     alert('Your message could not be sent.');
-      //     console.log(error);
-      //   }
-      // );
-    });
+    // console.log('reservation_date:', this.reservation_date.value);
+    // console.log('reservation_time:', this.reservation_time.value);
+    // console.log('reservation_guests:', this.reservation_guests.value);
+    emailjs.sendForm('service_azure', 'template_confirm', this).then(
+      function () {
+        alert('Your message has been sent!');
+        window.location.href = '/reserve/';
+      },
+      function (error) {
+        alert('Your message could not be sent.');
+        console.log(error);
+      }
+    );
+  });
 };
 
-const form = document.getElementById('contact-form');
+const form = document.getElementById('form');
 const submitButton = document.getElementById('submit-button');
 
 form.addEventListener('keydown', function (e) {
