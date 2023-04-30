cd4fb51f259ff06353c4d3c68b3d976ab62c49f2
julius <julius.soltani@gmail.com>
2023-04-30 15:53:36 +0200

    Fixed the urls and removed files and codes that were not working or being used

--- azure_bistro/urls.py
+++ azure_bistro/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('reserve/', include('reservations.urls')),
+    path('', include('reservations.urls')),
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -1,9 +1,7 @@
 import random
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import render, redirect
 from django.conf import settings
-from django.urls import reverse
 from .models import Reservation
-from django.http import Http404
 
 def reserve(request):
     if request.method == 'POST':
@@ -28,43 +26,12 @@ def reserve_contact(request):
     }
     return render(request, 'reservations/reserve_contact.html', context)
 
-def admin_view(request):
-    # Add authentication check
-    reservations = Reservation.objects.all().order_by('-date', '-time')
-    return render(request, 'reservations/admin_view.html', {'reservations': reservations})
 
 # Modifying Reservation
-def manage_reservation(request, reservation_number):
-    try:
-        reservation = Reservation.objects.get(reservation_number=reservation_number)
-    except Reservation.DoesNotExist:
-        raise Http404("Reservation does not exist")
-
+def manage_reservation(request):
+    context = {
+        'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
+    }
     # Render the reservation management page and pass the reservation data
-    return render(request, 'reservations/manage_reservation.html', {'reservation': reservation})
+    return render(request, 'reservations/manage_reservation.html', context)
 
-def update_reservation(request, reservation_number):
-    reservation = get_object_or_404(Reservation, reservation_number=reservation_number)
-    
-    if request.method == 'POST':
-        date = request.POST.get('date')
-        time = request.POST.get('time')
-        guests = request.POST.get('guests')
-
-        reservation.date = date
-        reservation.time = time
-        reservation.guests = guests
-        reservation.save()
-
-        return redirect(reverse('manage_reservation', args=[reservation_number]))
-    
-    return redirect('reserve')
-
-def cancel_reservation(request, reservation_number):
-    reservation = get_object_or_404(Reservation, reservation_number=reservation_number)
-
-    if request.method == 'POST':
-        reservation.delete()
-        return redirect('reserve')
-
-    return redirect('reserve')
